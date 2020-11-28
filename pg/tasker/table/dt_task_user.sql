d3cb43f5bc5eef7406457ebc79b64d387133a9fd
Gregory Siems <gsiems@gmail.com>
2020-11-28 17:46:19 -0600

    Add permissions column

--- pg/tasker/table/dt_task_user.sql
+++ pg/tasker/table/dt_task_user.sql
@@ -3,6 +3,9 @@ SET search_path = tasker, pg_catalog ;
 CREATE TABLE dt_task_user (
     task_id integer NOT NULL,
     user_id integer NOT NULL,
+    permission_id integer NOT NULL,
+    -- TODO: role ?? ( PM, BA, Developer, Tester, etc. )
+    last_viewed_dt timestamp with time zone,
     created_by integer,
     created_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
     CONSTRAINT dt_task_user_pk PRIMARY KEY ( task_id, user_id ) ) ;
@@ -14,6 +17,10 @@ COMMENT ON TABLE dt_task_user IS 'Team members assigned to tasks.' ;
 COMMENT ON COLUMN dt_task_user.task_id IS 'The ID of the task' ;
 
 COMMENT ON COLUMN dt_task_user.user_id IS 'The team member assigned to the task.' ;
+
+COMMENT ON COLUMN dt_task_user.permission_id IS 'The ID of the permission for the user.' ;
+
+COMMENT ON COLUMN dt_task_user.last_viewed_dt IS 'The timestamp when the task was last viewed by the user.' ;
 
 COMMENT ON COLUMN dt_task_user.created_by IS 'The ID of the individual that created the row (ref dt_user).' ;
 
@@ -28,5 +35,10 @@ ALTER TABLE dt_task_user
     ADD CONSTRAINT dt_task_user_fk02
     FOREIGN KEY ( user_id )
     REFERENCES dt_user ( id ) ;
+
+ALTER TABLE dt_task_user
+    ADD CONSTRAINT dt_task_user_fk03
+    FOREIGN KEY ( permission_id )
+    REFERENCES st_permission ( id ) ;
 
 REVOKE ALL ON TABLE dt_task_user FROM public ;
