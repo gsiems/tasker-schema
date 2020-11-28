40febfaad8491d35aca1e1bf2bb66526fbcd8ae7
Gregory Siems <gsiems@gmail.com>
2020-11-28 17:44:21 -0600

    Cleanup commented out columns. Remove scheduled_start (use column(s) from dt_task)

--- pg/tasker/table/dt_task_meeting.sql
+++ pg/tasker/table/dt_task_meeting.sql
@@ -2,10 +2,7 @@ SET search_path = tasker, pg_catalog ;
 
 CREATE TABLE dt_task_meeting (
     task_id integer NOT NULL,
-    --markup_type_id integer,
     meeting_location varchar ( 100 ),
-    scheduled_start timestamp with time zone,
-    --scheduled_duration interval,
     agenda_markup text,
     agenda_html text,
     minutes_markup text,
@@ -22,13 +19,7 @@ COMMENT ON TABLE dt_task_meeting IS 'Meeting specific data.' ;
 
 COMMENT ON COLUMN dt_task_meeting.task_id IS 'The unique ID for the task.' ;
 
---COMMENT ON COLUMN dt_task_meeting.markup_type_id IS 'The ID of the markup format used for the agenda and minutes.' ;
-
 COMMENT ON COLUMN dt_task_meeting.meeting_location IS 'The location of the meeting.' ;
-
-COMMENT ON COLUMN dt_task_meeting.scheduled_start IS 'The scheduled start date/time for the meeting.' ;
-
---COMMENT ON COLUMN dt_task_meeting.scheduled_duration IS 'The scheduled length of the meeting.' ;
 
 COMMENT ON COLUMN dt_task_meeting.agenda_markup IS 'The markup text of the meeting agenda.' ;
 
@@ -50,10 +41,5 @@ ALTER TABLE dt_task_meeting
     ADD CONSTRAINT dt_task_meeting_fk01
     FOREIGN KEY ( task_id )
     REFERENCES dt_task ( id ) ;
-
---ALTER TABLE dt_task_meeting
---    ADD CONSTRAINT dt_task_meeting_fk02
---    FOREIGN KEY ( markup_type_id )
---    REFERENCES st_markup_type ( id ) ;
 
 REVOKE ALL ON TABLE dt_task_meeting FROM public ;
