116f96713cf279cafd6900a05a4fefd4506179e1
Gregory Siems <gsiems@gmail.com>
2020-12-01 20:01:56 -0600

    update to reflect table/view changes

--- pg/08_create_tables.sql
+++ pg/08_create_tables.sql
@@ -1,18 +1,20 @@
 -- system tables
 \i tasker/table/st_markup_type.sql
 \i tasker/table/st_ranking.sql
-\i tasker/table/st_role.sql
 \i tasker/table/st_date_severity.sql
-\i tasker/table/st_task_category.sql
-\i tasker/table/st_visibility.sql
 \i tasker/table/st_issue_probability.sql
 \i tasker/table/st_issue_severity.sql
 \i tasker/table/st_issue_workaround.sql
+\i tasker/table/st_permission.sql
+--\i tasker/table/st_role.sql
+\i tasker/table/st_task_category.sql
+\i tasker/table/st_visibility.sql
 
 -- reference tables
-\i tasker/table/rt_activity_status.sql
-\i tasker/table/rt_activity_category.sql
-\i tasker/table/rt_activity_attribute_type.sql
+--\i tasker/table/rt_activity_status.sql
+--\i tasker/table/rt_activity_category.sql
+--\i tasker/table/rt_activity_attribute_type.sql
+\i tasker/table/rt_task_category_status.sql
 \i tasker/table/rt_task_status.sql
 \i tasker/table/rt_task_type.sql
 \i tasker/table/rt_task_attribute_type.sql
@@ -23,10 +25,10 @@
 \i tasker/table/dt_user_token.sql
 
 -- activity data tables
-\i tasker/table/dt_activity.sql
-\i tasker/table/dt_activity_user.sql
-\i tasker/table/dt_allowed_task_type.sql
-\i tasker/table/dt_activity_attribute.sql
+--\i tasker/table/dt_activity.sql
+--\i tasker/table/dt_activity_user.sql
+--\i tasker/table/dt_allowed_task_type.sql
+--\i tasker/table/dt_activity_attribute.sql
 
 -- task data tables
 \i tasker/table/dt_task.sql
@@ -39,7 +41,7 @@
 \i tasker/table/dt_task_file.sql
 
 -- task specialization tables
-\i tasker/table/dt_task_regular.sql
-\i tasker/table/dt_task_requirement.sql
+--\i tasker/table/dt_task_regular.sql
+--\i tasker/table/dt_task_requirement.sql
 \i tasker/table/dt_task_issue.sql
 \i tasker/table/dt_task_meeting.sql
