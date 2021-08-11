d4612cad662d6c306e085ae19d96ca3d3e17c10c
Gregory Siems <gsiems@gmail.com>
2021-08-10 20:40:24 -0500

    some cleanup

--- pg/08_create_tables.sql
+++ pg/08_create_tables.sql
@@ -8,13 +8,13 @@
 \i tasker/table/st_issue_severity.sql
 \i tasker/table/st_issue_workaround.sql
 \i tasker/table/st_permission.sql
---\i tasker/table/st_role.sql
 \i tasker/table/st_task_category.sql
 \i tasker/table/st_visibility.sql
 
 -- reference tables
-\i tasker/table/rt_task_type.sql
+\i tasker/table/rt_role.sql
 \i tasker/table/rt_task_status.sql
+\i tasker/table/rt_task_type.sql
 \i tasker/table/rt_task_category_status.sql
 \i tasker/table/rt_eav_attribute_type.sql
 \i tasker/table/rt_task_attribute_type.sql
@@ -29,6 +29,7 @@
 \i tasker/table/dt_task_association.sql
 \i tasker/table/dt_task_attribute.sql
 \i tasker/table/dt_task_user.sql
+
 \i tasker/table/dt_task_comment.sql
 \i tasker/table/dt_task_journal.sql
 \i tasker/table/dt_task_file.sql
