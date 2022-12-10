2250a6430b7f761fd5fdab0673e492652ca39d1b
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:33:25 -0600

    Added is_default, is_enabled, description, and ranking_id columns.

--- pg/tasker_data/table/st_date_severity.sql
+++ pg/tasker_data/table/st_date_severity.sql
@@ -1,8 +1,17 @@
 CREATE TABLE tasker_data.st_date_severity (
     id int2 NOT NULL,
+    ranking_id int2 NOT NULL,
     name text NOT NULL,
+    description text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
     CONSTRAINT st_date_severity_pk PRIMARY KEY ( id ),
     CONSTRAINT st_date_severity_nk UNIQUE ( name ) ) ;
+
+ALTER TABLE tasker_data.st_date_severity
+    ADD CONSTRAINT st_date_severity_fk01
+    FOREIGN KEY ( ranking_id )
+    REFERENCES tasker_data.st_ranking ( id ) ;
 
 ALTER TABLE tasker_data.st_date_severity OWNER TO tasker_owner ;
 
@@ -10,12 +19,24 @@ COMMENT ON TABLE tasker_data.st_date_severity IS 'Reference table. For estimated
 
 COMMENT ON COLUMN tasker_data.st_date_severity.id IS 'Unique ID/value for the severity level.' ;
 
-COMMENT ON COLUMN tasker_data.st_date_severity.name IS 'Display name for the severity level.' ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.ranking_id IS 'The priority ranking associated with the data severity.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_date_severity FROM public ;
+COMMENT ON COLUMN tasker_data.st_date_severity.name IS 'Display name for the date severity level.' ;
 
-INSERT INTO tasker_data.st_date_severity (id, name) VALUES (1, 'Aspirational: Missing the date has no implications.') ;
-INSERT INTO tasker_data.st_date_severity (id, name) VALUES (4, 'Minor: Missing the date has minor implications.') ;
-INSERT INTO tasker_data.st_date_severity (id, name) VALUES (6, 'Average: Missing the date has implications.') ;
-INSERT INTO tasker_data.st_date_severity (id, name) VALUES (7, 'Major: Missing the date has major implications.') ;
-INSERT INTO tasker_data.st_date_severity (id, name) VALUES (8, 'Critical: Missing the date has critical implications.') ;
+COMMENT ON COLUMN tasker_data.st_date_severity.description IS 'Description of the date severity level.' ;
+
+COMMENT ON COLUMN tasker_data.st_date_severity.is_default IS 'Indicates whether or not the row is the default row.' ;
+
+COMMENT ON COLUMN tasker_data.st_date_severity.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_date_severity (
+        id,
+        name,
+        description,
+        is_default )
+    VALUES
+         (1, 1, 'Aspirational', 'Missing the date has no negative implications.', true ),
+         (2, 3, 'Minor', 'Missing the date has minor implications.', false ),
+         (3, 5, 'Average', 'Missing the date has implications.', false ),
+         (4, 7, 'Major', 'Missing the date has major implications.', false ),
+         (5, 9, 'Critical', 'Missing the date has critical implications.', false ) ;
