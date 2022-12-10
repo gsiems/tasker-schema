55af9febcd3c125efc31b8a76da432e095fee071
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:34:26 -0600

    Added is_default, is_enabled columns.

--- pg/tasker_data/table/st_association_type.sql
+++ pg/tasker_data/table/st_association_type.sql
@@ -2,6 +2,8 @@ CREATE TABLE tasker_data.st_association_type (
     id int2 NOT NULL,
     name text NOT NULL,
     description text,
+    is_default boolean default false,
+    is_enabled boolean default true,
     CONSTRAINT st_association_type_pk PRIMARY KEY ( id ),
     CONSTRAINT st_association_type_nk UNIQUE ( name ) ) ;
 
@@ -13,10 +15,18 @@ COMMENT ON COLUMN tasker_data.st_association_type.id IS 'Unique ID for a type of
 
 COMMENT ON COLUMN tasker_data.st_association_type.name IS 'The name for a type of task association.' ;
 
-COMMENT ON COLUMN tasker_data.st_association_type.description IS 'The description of a task associations.' ;
+COMMENT ON COLUMN tasker_data.st_association_type.description IS 'The description of a task association.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_association_type FROM public ;
+COMMENT ON COLUMN tasker_data.st_association_type.is_default IS 'Indicates whether or not the row is the default row.' ;
 
-INSERT INTO tasker_data.st_association_type (id, name, description) VALUES (1, 'Association', 'Task has an association with another task.');
-INSERT INTO tasker_data.st_association_type (id, name, description) VALUES (2, 'Dependency', 'Task has a dependency on another task.');
-INSERT INTO tasker_data.st_association_type (id, name, description) VALUES (3, 'Duplicate', 'Task is a duplicate of another task.');
+COMMENT ON COLUMN tasker_data.st_association_type.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_association_type (
+        id,
+        name,
+        description,
+        is_default )
+    VALUES
+        ( 1, 'Association', 'Task has an association with another task.', true ),
+        ( 2, 'Dependency', 'Task has a dependency on another task.', false ),
+        ( 3, 'Duplicate', 'Task is a duplicate of another task.', false ) ;
