877ab0324a1d1c0ff85c3c54f6c4b4a31e2fe94f
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:04:43 -0600

    Added is_default, is_enabled columns

--- pg/tasker_data/table/st_task_category.sql
+++ pg/tasker_data/table/st_task_category.sql
@@ -2,6 +2,8 @@ CREATE TABLE tasker_data.st_task_category (
     id int2 NOT NULL,
     name text NOT NULL,
     description text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
     CONSTRAINT st_task_category_pk PRIMARY KEY ( id ),
     CONSTRAINT st_task_category_nk UNIQUE ( name ) ) ;
 
@@ -15,11 +17,17 @@ COMMENT ON COLUMN tasker_data.st_task_category.name IS 'The name for a task cate
 
 COMMENT ON COLUMN tasker_data.st_task_category.description IS 'The description of a task category.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_task_category FROM public ;
+COMMENT ON COLUMN tasker_data.st_task_category.is_default IS 'Indicates whether or not the row is the default row.' ;
 
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (1, 'Task', 'Task category for tasks that do not fall in any other category.');
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (2, 'Requirement', 'Special task category for requirements.');
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (3, 'Issue', 'Special task category for issues.');
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (4, 'Meeting', 'Special task category for meetings.');
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (5, 'Activity', 'Special task category for activities.');
-INSERT INTO tasker_data.st_task_category (id, name, description) VALUES (6, 'PIP', 'Special task category for "Points or Intervals of Progress".');
+COMMENT ON COLUMN tasker_data.st_task_category.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_task_category (
+        name,
+        description )
+    VALUES
+        ( 'Task', 'Task category for tasks that do not fall in any other category.' ),
+        ( 'Requirement', 'Special task category for requirements.' ),
+        ( 'Issue', 'Special task category for issues.' ),
+        ( 'Meeting', 'Special task category for meetings.' ),
+        ( 'Activity', 'Special task category for activities.' ),
+        ( 'PIP', 'Special task category for "Points or Intervals of Progress".' ) ;
