db438036da9eb7382f36171f8bdb1a6af2c29ccc
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:16:46 -0600

    Added is_default, is_enabled columns. Rename priority_id column.

--- pg/tasker_data/table/st_issue_workaround.sql
+++ pg/tasker_data/table/st_issue_workaround.sql
@@ -1,29 +1,43 @@
 CREATE TABLE tasker_data.st_issue_workaround (
     id int2 NOT NULL,
-    priority_id int2 NOT NULL,
+    ranking_id int2 NOT NULL,
     name text NOT NULL,
+    description text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
     CONSTRAINT st_issue_workaround_pk PRIMARY KEY ( id ),
     CONSTRAINT st_issue_workaround_nk UNIQUE ( name ) ) ;
+
+ALTER TABLE tasker_data.st_issue_workaround
+    ADD CONSTRAINT st_issue_workaround_fk01
+    FOREIGN KEY ( ranking_id )
+    REFERENCES tasker_data.st_ranking ( id ) ;
 
 ALTER TABLE tasker_data.st_issue_workaround OWNER TO tasker_owner ;
 
 COMMENT ON TABLE tasker_data.st_issue_workaround IS 'Reference table. The type of workarounds available for an issue.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_workaround.id IS 'Unique ID/value for the issue_workaround.' ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.id IS 'Unique ID/value for the workaround.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_workaround.priority_id IS 'The priority associated with the workaround.' ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.ranking_id IS 'The priority ranking associated with the workaround.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_workaround.name IS 'Display name for the issue_workaround.' ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.name IS 'Display name for the workaround.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_issue_workaround FROM public ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.description IS 'Description of the workaround.' ;
 
-ALTER TABLE tasker_data.st_issue_workaround
-    ADD CONSTRAINT st_issue_workaround_fk01
-    FOREIGN KEY ( priority_id )
-    REFERENCES tasker_data.st_ranking ( id ) ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.is_default IS 'Indicates whether or not the row is the default row.' ;
 
-INSERT INTO tasker_data.st_issue_workaround (id, priority_id, name) VALUES (1, 1, 'Not applicable/not required') ;
-INSERT INTO tasker_data.st_issue_workaround (id, priority_id, name) VALUES (2, 2, 'An easy/obvious workaround exists') ;
-INSERT INTO tasker_data.st_issue_workaround (id, priority_id, name) VALUES (3, 4, 'A reasonable/not difficult workaround exists') ;
-INSERT INTO tasker_data.st_issue_workaround (id, priority_id, name) VALUES (4, 6, 'A difficult/non-obvious workaround exists') ;
-INSERT INTO tasker_data.st_issue_workaround (id, priority_id, name) VALUES (5, 8, 'There is no known workaround') ;
+COMMENT ON COLUMN tasker_data.st_issue_workaround.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_issue_workaround (
+        id,
+        ranking_id,
+        name,
+        description,
+        is_default )
+    VALUES
+        ( 1, 1, 'Not applicable', 'No workaround is required', true ),
+        ( 2, 2, 'Easy', 'AN easy/obvious workaround exists', false ),
+        ( 3, 4, 'Reasonable', 'A reasonable/not difficult workaround exists', false ),
+        ( 4, 6, 'Difficult', 'A difficult/non-obvious workaround exists', false ),
+        ( 5, 8, 'None', 'There is no known workaround', false ) ;
