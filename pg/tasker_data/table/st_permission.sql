6cf81a536493909e446887fbe62761c996dcbe88
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:06:23 -0600

    Added is_default, is_enabled columns

--- pg/tasker_data/table/st_permission.sql
+++ pg/tasker_data/table/st_permission.sql
@@ -1,9 +1,11 @@
 CREATE TABLE tasker_data.st_permission (
     id int2 NOT NULL,
-    can_create_task boolean DEFAULT false NOT NULL,
-    can_update_task boolean DEFAULT false NOT NULL,
     name text NOT NULL,
     description text,
+    can_create_task boolean DEFAULT false NOT NULL,
+    can_update_task boolean DEFAULT false NOT NULL,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
     CONSTRAINT st_permission_pk PRIMARY KEY ( id ),
     CONSTRAINT st_permission_nk UNIQUE ( name ) ) ;
 
@@ -13,17 +15,24 @@ COMMENT ON TABLE tasker_data.st_permission IS 'User permissions.' ;
 
 COMMENT ON COLUMN tasker_data.st_permission.id IS 'Unique ID for a permission.' ;
 
-COMMENT ON COLUMN tasker_data.st_permission.can_create_task IS 'Indicates whether or not the permission is allowed to create sub-tasks.' ;
-
-COMMENT ON COLUMN tasker_data.st_permission.can_update_task IS 'Indicates whether or not the permission is allowed to update a task.' ;
-
 COMMENT ON COLUMN tasker_data.st_permission.name IS 'The name of the permission.' ;
 
 COMMENT ON COLUMN tasker_data.st_permission.description IS 'The description of the permission.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_permission FROM public ;
+COMMENT ON COLUMN tasker_data.st_permission.can_create_task IS 'Indicates whether or not the permission is allowed to create sub-tasks.' ;
 
-INSERT INTO tasker_data.st_permission (id, name, can_create_task, can_update_task) VALUES (2, 'All', true, true) ;
-INSERT INTO tasker_data.st_permission (id, name, can_create_task, can_update_task) VALUES (3, 'Update', false, true) ;
-INSERT INTO tasker_data.st_permission (id, name, can_create_task, can_update_task) VALUES (4, 'Create', true, false) ;
-INSERT INTO tasker_data.st_permission (id, name, can_create_task, can_update_task) VALUES (5, 'Read', false, false) ;
+COMMENT ON COLUMN tasker_data.st_permission.can_update_task IS 'Indicates whether or not the permission is allowed to update a task.' ;
+
+COMMENT ON COLUMN tasker_data.st_permission.is_default IS 'Indicates whether or not the row is the default row.' ;
+
+COMMENT ON COLUMN tasker_data.st_permission.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_permission (
+        name,
+        can_create_task,
+        can_update_task )
+    VALUES
+        ( 'All', true, true ),
+        ( 'Update', false, true ),
+        ( 'Create', true, false ),
+        ( 'Read', false, false ) ;
