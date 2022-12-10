4bc880c934eb26b4e5688a0b105a97ac0ba6f157
Gregory Siems <gsiems@gmail.com>
2022-12-10 09:26:54 -0600

    Added is_default, is_enabled, and description columns. Rename priority_id column.

--- pg/tasker_data/table/st_issue_probability.sql
+++ pg/tasker_data/table/st_issue_probability.sql
@@ -1,30 +1,43 @@
 CREATE TABLE tasker_data.st_issue_probability (
     id int2 NOT NULL,
-    priority_id int2 NOT NULL,
+    ranking_id int2 NOT NULL,
     name text NOT NULL,
+    description text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
     CONSTRAINT st_issue_probability_pk PRIMARY KEY ( id ),
     CONSTRAINT st_issue_probability_nk UNIQUE ( name ) ) ;
+
+ALTER TABLE tasker_data.st_issue_probability
+    ADD CONSTRAINT st_issue_probability_fk01
+    FOREIGN KEY ( ranking_id )
+    REFERENCES tasker_data.st_ranking ( id ) ;
 
 ALTER TABLE tasker_data.st_issue_probability OWNER TO tasker_owner ;
 
 COMMENT ON TABLE tasker_data.st_issue_probability IS 'Reference table. Probability/repeatability of triggering the issue.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_probability.id IS 'Unique ID/value for the issue_probability.' ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.id IS 'Unique ID/value for the probability.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_probability.priority_id IS 'The priority associated with the probability.' ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.ranking_id IS 'The priority ranking associated with the severity.' ;
 
-COMMENT ON COLUMN tasker_data.st_issue_probability.name IS 'Display name for the issue_probability.' ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.name IS 'Display name for the probability.' ;
 
-REVOKE ALL ON TABLE tasker_data.st_issue_probability FROM public ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.description IS 'Description of the probability.' ;
 
-ALTER TABLE tasker_data.st_issue_probability
-    ADD CONSTRAINT st_issue_probability_fk01
-    FOREIGN KEY ( priority_id )
-    REFERENCES tasker_data.st_ranking ( id ) ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.is_default IS 'Indicates whether or not the row is the default row.' ;
 
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (1, 1, 'Not applicable') ;
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (2, 2, 'Unknown') ;
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (3, 3, 'Random') ;
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (4, 4, 'Rarely') ;
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (5, 5, 'Sometimes') ;
-INSERT INTO tasker_data.st_issue_probability (id, priority_id, name) VALUES (6, 8, 'Always') ;
+COMMENT ON COLUMN tasker_data.st_issue_probability.is_enabled IS 'Indicates whether or not the row is available for new use.' ;
+
+INSERT INTO tasker_data.st_issue_probability (
+        id,
+        name,
+        description,
+        is_default )
+    VALUES
+        ( 1, 1, 'Not applicable', '', true ),
+        ( 2, 2, 'Unknown', '', false ),
+        ( 4, 4, 'Rarely', '', false ),
+        ( 5, 6, 'Sometimes', '', false ),
+        ( 6, 8, 'Often', '', false ),
+        ( 7, 9, 'Always', '', false ) ;
