f0d39ffb9eacb1e8b0d04ef64a38e406557a7194
Gregory Siems <gsiems@gmail.com>
2022-12-11 09:49:27 -0600

    Fix insert statement.

--- pg/tasker_data/table/st_task_category.sql
+++ pg/tasker_data/table/st_task_category.sql
@@ -23,11 +23,12 @@ COMMENT ON COLUMN tasker_data.st_task_category.is_enabled IS 'Indicates whether 
 
 INSERT INTO tasker_data.st_task_category (
         name,
-        description )
+        description,
+        is_default )
     VALUES
-        ( 'Task', 'Task category for tasks that do not fall in any other category.' ),
-        ( 'Requirement', 'Special task category for requirements.' ),
-        ( 'Issue', 'Special task category for issues.' ),
-        ( 'Meeting', 'Special task category for meetings.' ),
-        ( 'Activity', 'Special task category for activities.' ),
-        ( 'PIP', 'Special task category for "Points or Intervals of Progress".' ) ;
+        ( 1, 'Other', 'Task category for tasks that do not fall in any other category.', true ),
+        ( 2, 'Requirement', 'Special task category for requirements.', false ),
+        ( 3, 'Issue', 'Special task category for issues.', false ),
+        ( 4, 'Meeting', 'Special task category for meetings.', false ),
+        ( 5, 'Activity', 'Special task category for activities.', false ),
+        ( 6, 'PIP', 'Special task category for "Points or Intervals of Progress".', false ) ;
