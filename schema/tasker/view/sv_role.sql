4c57e104ff5de58fb48943a02abce73f83dbabb7
Gregory Siems <gsiems@gmail.com>
2022-12-18 12:29:38 -0600

    update to reflect changes to base table

--- schema/tasker/view/sv_role.sql
+++ schema/tasker/view/sv_role.sql
@@ -3,7 +3,6 @@ AS
 SELECT base.id,
         base.name,
         base.description,
-        base.is_activity_role,
         base.is_default,
         base.is_enabled
     FROM tasker_data.st_role base ;
@@ -16,6 +15,5 @@ COMMENT ON VIEW tasker.sv_role IS 'View of: Reference table. Roles that may be a
 COMMENT ON COLUMN tasker.sv_role.id IS 'Unique ID for the role.' ;
 COMMENT ON COLUMN tasker.sv_role.name IS 'The name for the role.' ;
 COMMENT ON COLUMN tasker.sv_role.description IS 'The description of the role.' ;
-COMMENT ON COLUMN tasker.sv_role.is_activity_role IS 'Indicates whether or not the role is for activities.' ;
 COMMENT ON COLUMN tasker.sv_role.is_default IS 'Indicates whether or not the role is the default role.' ;
 COMMENT ON COLUMN tasker.sv_role.is_enabled IS 'Indicates whether or not the role is available for new use.' ;
