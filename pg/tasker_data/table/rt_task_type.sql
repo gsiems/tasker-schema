013a27feec52a8985a94525235b95853044677fa
Gregory Siems <gsiems@gmail.com>
2022-12-17 21:13:29 -0600

    Add is_default column, undo column tetris, update inserts

--- pg/tasker_data/table/rt_task_type.sql
+++ pg/tasker_data/table/rt_task_type.sql
@@ -1,18 +1,29 @@
 CREATE TABLE tasker_data.rt_task_type (
-    created_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
-    updated_dt timestamp with time zone,
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    created_by_id integer,
-    updated_by_id integer,
-    category_id int2 NOT NULL default 1,
+    category_id int2 NOT NULL,
     markup_type_id int2 NOT NULL default 1,
-    is_enabled boolean DEFAULT true NOT NULL,
     name text NOT NULL,
     description text,
     template_markup text,
     template_html text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean DEFAULT true NOT NULL,
+    created_by_id integer,
+    updated_by_id integer,
+    created_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
+    updated_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
     CONSTRAINT rt_task_type_pk PRIMARY KEY ( id ),
     CONSTRAINT rt_task_type_nk UNIQUE ( category_id, name ) ) ;
+
+ALTER TABLE tasker_data.rt_task_type
+    ADD CONSTRAINT rt_task_type_fk01
+    FOREIGN KEY ( category_id )
+    REFERENCES tasker_data.st_task_category ( id ) ;
+
+ALTER TABLE tasker_data.rt_task_type
+    ADD CONSTRAINT rt_task_type_fk02
+    FOREIGN KEY ( markup_type_id )
+    REFERENCES tasker_data.st_markup_type ( id ) ;
 
 ALTER TABLE tasker_data.rt_task_type OWNER TO tasker_owner ;
 
@@ -42,66 +53,131 @@ COMMENT ON COLUMN tasker_data.rt_task_type.updated_by_id IS 'The ID of the indiv
 
 COMMENT ON COLUMN tasker_data.rt_task_type.updated_dt IS 'The timestamp when the row was most recently updated.' ;
 
-REVOKE ALL ON TABLE tasker_data.rt_task_type FROM public ;
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'Task', 'Action item', 'Task type for an assigned action item.' ),
+                ( 'Task', 'Administration', 'Task type for administrative actions.' ),
+                ( 'Task', 'Corrective action', 'Task type for performing corrective actions.' ),
+                ( 'Task', 'Discovery', 'Task type for performing discovery.' ),
+                ( 'Task', 'Event', 'Task type for an event.' ),
+                ( 'Task', 'Issue management', 'Task type for evaluating/managing issues' ),
+                ( 'Task', 'Maintenance', 'Task type for performing maintenance tasks' ),
+                ( 'Task', 'Meeting preparation', 'Task type for setting up/preparing for meetings' ),
+                ( 'Task', 'Other', '' ),
+                ( 'Task', 'Personal Project', 'Task type for a personal project' ),
+                ( 'Task', 'Project management', 'Task type for overhead of actually managing projects' ),
+                ( 'Task', 'Requirement development', 'Task type for developing requirements.' ),
+                ( 'Task', 'Requirement implementation', 'Task type for implementing a requirement.' ),
+                ( 'Task', 'Research', 'Task type for performing research.' ),
+                ( 'Task', 'Test development', 'Task type for developing tests' ),
+                ( 'Task', 'Testing', 'Task type for performing tests' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
 
-ALTER TABLE tasker_data.rt_task_type
-    ADD CONSTRAINT rt_task_type_fk01
-    FOREIGN KEY ( category_id )
-    REFERENCES tasker_data.st_task_category ( id ) ;
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'Requirement', 'Business', 'Task type for business requirements' ),
+                ( 'Requirement', 'Functional', 'Task type for functional requirements' ),
+                ( 'Requirement', 'Process', 'Task type for process requirements' ),
+                ( 'Requirement', 'Style', 'Task type for "Having to do with the appearance (vs. substance) of" requirements.' ),
+                ( 'Requirement', 'Technical', 'Task type for technical requirements' ),
+                ( 'Requirement', 'Other', 'Task type for other kinds of requirements' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
 
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Action item', 'Task type for an assigned action item.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Administration', 'Task type for administrative actions.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Corrective action', 'Task type for performing corrective actions.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Discovery', 'Task type for performing discovery.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Event', 'Task type for an event.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Issue management', 'Task type for evaluating/managing issues');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Maintenance', 'Task type for performing maintenance tasks');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Meeting preparation', 'Task type for setting up/preparing for meetings');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Other', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Personal Project', 'Task type for a personal project');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Project management', 'Task type for overhead of actually managing projects');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Requirement development', 'Task type for developing requirements.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Requirement implementation', 'Task type for implementing a requirement.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Research', 'Task type for performing research.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Test development', 'Task type for developing tests');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (1, 'Testing', 'Task type for performing tests');
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'Issue', 'Defect', 'A problem that prevents or impaires current, or required, functionality.' ),
+                ( 'Issue', 'Enhancement', 'Task type for requesting an improvement to the current functionality.' ),
+                ( 'Issue', 'New feature', 'Task type for requesting a new feature or capability.' ),
+                ( 'Issue', 'Other', 'Task type for other kinds of issues.' ),
+                ( 'Issue', 'Risk', 'A potential problem that may prevent or impair current, or required, functionality or that may otherwise endanger the activity.' ),
+                ( 'Issue', 'Support request', 'Task type for support request issues' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
 
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Business', 'Task type for business requirements');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Functional', 'Task type for functional requirements');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Process', 'Task type for process requirements');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Style', 'Task type for "Having to do with the appearance (vs. substance) of" requirements.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Technical', 'Task type for technical requirements');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (2, 'Other', 'Task type for other kinds of requirements');
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'Meeting', 'Meeting', 'Task type for conducting/attending meetings.' ),
+                ( 'Meeting', 'Pre-meeting meeting', 'Task type for conducting/attending meetings prior to/in preparation for a larger meeting.' ),
+                ( 'Meeting', 'Event', 'Task type for an event.' ),
+                ( 'Meeting', 'Ad hoc', 'Special purpose meeting (not specified)' ),
+                ( 'Meeting', 'Kickoff', 'Activity start' ),
+                ( 'Meeting', 'Management', '' ),
+                ( 'Meeting', 'Staff', 'Meeting between a manager and their staff' ),
+                ( 'Meeting', 'One-on-one', 'Meeting between two individuals' ),
+                ( 'Meeting', 'Stand-up', '' ),
+                ( 'Meeting', 'Status', '' ),
+                ( 'Meeting', 'Team', '' ),
+                ( 'Meeting', 'Work', 'Meeting (not otherwise specified) which produces a product or result.' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
 
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'Defect', 'A problem that prevents or impaires current, or required, functionality.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'Enhancement', 'Task type for requesting an improvement to the current functionality.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'New feature', 'Task type for requesting a new feature or capability.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'Other', 'Task type for other kinds of issues.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'Risk', 'A potential problem that may prevent or impair current, or required, functionality or that may otherwise endanger the activity.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (3, 'Support request', 'Task type for support request issues');
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'Activity', 'Default', 'Default activity type.' ),
+                ( 'Activity', 'Project', 'Activity type for projects.' ),
+                ( 'Activity', 'Research project', 'Activity type for research projects.' ),
+                ( 'Activity', 'Support', 'Activity type for support activities.' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
 
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Meeting', 'Task type for conducting/attending meetings.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Pre-meeting meeting', 'Task type for conducting/attending meetings prior to/in preparation for a larger meeting.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Event', 'Task type for an event.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Ad hoc', 'Special purpose meeting (not specified)');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Kickoff', 'Activity start');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Management', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Staff', 'Meeting between a manager and their staff');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'One-on-one', 'Meeting between two indviduals');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Stand-up', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Status', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Team', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (4, 'Work', 'Meeting (not otherwise specified) which produces a product or result.');
-
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (5, 'Default', 'Default activity type.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (5, 'Project', 'Activity type for projects.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (5, 'Research project', 'Activity type for research projects.');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (5, 'Support', 'Activity type for support activities.');
-
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Milestone', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Phase', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Release', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Revision', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Scrum Spike', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Scrum Sprint', '');
-INSERT INTO tasker_data.rt_task_type (category_id, name, description) VALUES (6, 'Version', '');
+INSERT INTO tasker_data.rt_task_type (
+        category_id,
+        name,
+        description )
+    SELECT stc.id AS category_id,
+            dat.name,
+            dat.description
+        FROM tasker_data.st_task_category stc
+        JOIN (
+            VALUES
+                ( 'PIP', 'Milestone', '' ),
+                ( 'PIP', 'Phase', '' ),
+                ( 'PIP', 'Release', '' ),
+                ( 'PIP', 'Revision', '' ),
+                ( 'PIP', 'Scrum Spike', '' ),
+                ( 'PIP', 'Scrum Sprint', '' ),
+                ( 'PIP', 'Version', '' )
+            ) AS dat ( category_name, name, description )
+            ON ( dat.category_name = stc.name ) ;
