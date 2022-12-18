9be488fe7dd2b0d801e6ec55f4433f5b548a0a91
Gregory Siems <gsiems@gmail.com>
2022-12-17 21:15:21 -0600

    Add is_default column, undo column tetris

--- pg/tasker_data/table/rt_role.sql
+++ pg/tasker_data/table/rt_role.sql
@@ -1,12 +1,13 @@
 CREATE TABLE tasker_data.rt_role (
-    created_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
-    updated_dt timestamp with time zone,
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    created_by_id integer,
-    updated_by_id integer,
-    is_enabled boolean DEFAULT true NOT NULL,
     name text NOT NULL,
     description text,
+    is_default boolean NOT NULL default false,
+    is_enabled boolean NOT NULL default true,
+    created_by_id integer,
+    updated_by_id integer,
+    created_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
+    updated_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
     CONSTRAINT rt_role_pk PRIMARY KEY ( id ),
     CONSTRAINT dt_task_role_nk UNIQUE ( name ) ) ;
 
@@ -20,6 +21,8 @@ COMMENT ON COLUMN tasker_data.rt_role.name IS 'The name of a role.' ;
 
 COMMENT ON COLUMN tasker_data.rt_role.description IS 'The description of the role.' ;
 
+COMMENT ON COLUMN tasker_data.rt_role.is_default IS 'Indicates whether or not the role is the default role.' ;
+
 COMMENT ON COLUMN tasker_data.rt_role.is_enabled IS 'Indicates whether or not the role is available for use.' ;
 
 COMMENT ON COLUMN tasker_data.rt_role.created_by_id IS 'The ID of the individual that created the row (ref dt_user).' ;
@@ -30,6 +33,7 @@ COMMENT ON COLUMN tasker_data.rt_role.updated_by_id IS 'The ID of the individual
 
 COMMENT ON COLUMN tasker_data.rt_role.updated_dt IS 'The timestamp when the row was most recently updated.' ;
 
-REVOKE ALL ON TABLE tasker_data.rt_role FROM public ;
-
-INSERT INTO tasker_data.rt_role (name, description) VALUES ('Default', 'The default role') ;
+INSERT INTO tasker_data.rt_role
+        (name, description, is_default )
+    VALUES
+        ('Default', 'The default role', true ) ;
