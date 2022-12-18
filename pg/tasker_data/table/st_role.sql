5d1542815341002666a4dfe40912185fd521647b
Gregory Siems <gsiems@gmail.com>
2022-12-17 22:09:35 -0600

    Remove is_activity_role column

--- pg/tasker_data/table/st_role.sql
+++ pg/tasker_data/table/st_role.sql
@@ -2,9 +2,8 @@ CREATE TABLE tasker_data.st_role (
     id int2 NOT NULL,
     name text NOT NULL,
     description text,
-    is_activity_role boolean NOT NULL default true,
     is_default boolean NOT NULL default false,
-    is_enabled boolean NOT NULL default true,
+    is_enabled boolean NOT NULL default
     CONSTRAINT st_role_pk PRIMARY KEY ( id ),
     CONSTRAINT st_role_nk UNIQUE ( name ) ) ;
 
@@ -28,13 +27,11 @@ INSERT INTO tasker_data.st_role (
         id,
         name,
         description,
-        is_activity_role,
         is_default )
     VALUES
-        ( 10, 'Observer',           'Activity role for observers', true, true ),
-        ( 20, 'Reporter',           'Activity role for reporters', true, false ),
-        ( 30, 'Member',             'Activity role for working on activities', true, false ),
-        ( 40, 'Analyst',            'Activity role for (business) analysts', true, false ),
-        ( 50, 'Manager',            'Activity role for managing activities', true, false ),
-        ( 60, 'Administrator',      'Activity role for administering activities', true, false ),
-        ( 70, 'System Administrator', 'Role for administering top level activities, users, and reference data', false, false ) ;
+        ( 10, 'Observer',       'Activity role for observers', true ),
+        ( 20, 'Reporter',       'Activity role for reporters', false ),
+        ( 30, 'Member',         'Activity role for working on activities', false ),
+        ( 40, 'Analyst',        'Activity role for (business) analysts', false ),
+        ( 50, 'Manager',        'Activity role for managing activities', false ),
+        ( 60, 'Activity Owner', 'Activity role for administering activities', false ) ;
