ce8f0e129427b4e3667221fef4ef028132685114
Gregory Siems <gsiems@gmail.com>
2022-12-17 22:49:47 -0600

    Refresh/refactor view

--- pg/tasker/view/dv_user_reporting_chain.sql
+++ pg/tasker/view/dv_user_reporting_chain.sql
@@ -1,43 +1,37 @@
-SET search_path = tasker, pg_catalog ;
-
-CREATE VIEW dv_user_cc
+CREATE VIEW tasker.dv_user_reporting_chain
 AS
-WITH RECURSIVE coc AS (
+WITH RECURSIVE rcoc AS (
     SELECT p.id,
-            '{}'::int[] AS bosses,
-            p.reports_to,
+            '{}'::int[] AS reporting_chain,
+            p.supervisor_id,
             0 AS user_depth,
             ARRAY [ dense_rank () OVER (
                 ORDER BY p.id ) ] AS outln,
             ARRAY[p.id] AS user_path
-        FROM tasker.dt_user p
-        WHERE ( p.reports_to IS NULL )
+        FROM tasker_data.dt_user p
+        WHERE ( p.supervisor_id IS NULL )
     UNION ALL
     SELECT s.id,
-            bosses || s.reports_to,
-            s.reports_to,
+            reporting_chain || s.supervisor_id,
+            s.supervisor_id,
             ( q.user_depth + 1 ) AS user_depth,
             ( q.outln || dense_rank () OVER (
-                    PARTITION BY s.reports_to
+                    PARTITION BY s.supervisor_id
                     ORDER BY s.id ) ) AS outln,
             q.user_path || s.id
-        FROM tasker.dt_user s
-        JOIN coc q
-            ON ( s.reports_to = q.id
-                AND NOT s.id = ANY ( q.bosses ) )
+        FROM tasker_data.dt_user s
+        JOIN rcoc q
+            ON ( s.supervisor_id = q.id
+                AND NOT s.id = ANY ( q.reporting_chain ) )
 )
 SELECT id AS user_id,
-        bosses,
-        reports_to,
+        reporting_chain,
+        supervisor_id,
         user_depth,
         user_path,
         array_to_string ( outln, '.'::text ) AS user_outln
-    FROM coc ;
+    FROM rcoc ;
 
-ALTER TABLE dv_user_cc OWNER TO tasker_owner ;
+ALTER TABLE tasker.dv_user_reporting_chain OWNER TO tasker_owner ;
 
-COMMENT ON VIEW dv_user_cc IS 'Org chart (chain of command) for users.' ;
-
-REVOKE ALL ON TABLE dv_user_cc FROM public ;
-
-GRANT SELECT ON table dv_user_cc TO tasker_owner ;
+COMMENT ON VIEW tasker.dv_user_reporting_chain IS 'Org chart (reporting chain of command) for users.' ;
