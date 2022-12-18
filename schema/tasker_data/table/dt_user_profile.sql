e0ada28e72ac88bee93f9cf51582026da05c82ba
Gregory Siems <gsiems@gmail.com>
2022-12-18 12:30:45 -0600

    Add foreign key to dt_user table. Fix comments.

--- schema/tasker_data/table/dt_user_profile.sql
+++ schema/tasker_data/table/dt_user_profile.sql
@@ -10,24 +10,29 @@ CREATE TABLE tasker_data.dt_user_profile (
     updated_dt timestamp with time zone DEFAULT ( now () AT TIME ZONE 'UTC' ),
     CONSTRAINT dt_user_profile_pk PRIMARY KEY ( user_id ) ) ;
 
+ALTER TABLE tasker_data.dt_user_profile
+    ADD CONSTRAINT dt_user_profiles_fk01
+    FOREIGN KEY ( user_id )
+    REFERENCES tasker_data.dt_user ( id ) ;
+
 ALTER TABLE tasker_data.dt_user_profile OWNER TO tasker_owner ;
 
 COMMENT ON TABLE tasker_data.dt_user_profile IS 'User profiles for user accounts.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.user_id IS 'Unique ID for the user account.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.user_id IS 'Unique ID for the user account.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.edition IS 'Indicates the number of edits made to the user record. Intended for use in determining if a user has been edited between select and update.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.edition IS 'Indicates the number of edits made to the user record. Intended for use in determining if a user has been edited between select and update.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.full_name IS 'The name of the account user.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.full_name IS 'The name of the account user.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.email_address IS 'The email address for the account user.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.email_address IS 'The email address for the account user.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.email_is_enabled IS 'Indicates if the user wants to be notified (by email) when there is a change to an item that is associated with their account.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.email_is_enabled IS 'Indicates if the user wants to be notified (by email) when there is a change to an item that is associated with their account.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.created_by_id IS 'The ID of the individual that created the row (ref dt_user).' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.created_by_id IS 'The ID of the individual that created the row (ref dt_user).' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.created_dt IS 'The timestamp when the row was created.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.created_dt IS 'The timestamp when the row was created.' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.updated_by_id IS 'The ID of the individual that most recently updated the row (ref dt_user).' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.updated_by_id IS 'The ID of the individual that most recently updated the row (ref dt_user).' ;
 
-COMMENT ON COLUMN tasker_data.dt_user.updated_dt IS 'The timestamp when the row was most recently updated.' ;
+COMMENT ON COLUMN tasker_data.dt_user_profile.updated_dt IS 'The timestamp when the row was most recently updated.' ;
