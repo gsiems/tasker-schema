5d59e8973efee0e8fa407cc14ad23d2160d81706
Gregory Siems <gsiems@gmail.com>
2022-12-17 22:47:26 -0600

    Refresh activity view(s)

--- pg/tasker/view/dv_activity.sql
+++ pg/tasker/view/dv_activity.sql
@@ -1,91 +1,112 @@
-SET search_path = tasker, pg_catalog ;
-
-CREATE VIEW dv_activity
+CREATE OR REPLACE VIEW tasker.dv_activity
 AS
-SELECT dt.id AS activity_id,
-        dt.parent_id,
-        dt.edition,
-        toc.parents,
-        toc.task_depth,
-        toc.task_path,
-        toc.task_outln,
-        dt.task_name,
-        dt.visibility_id,
-        sv.name AS visbility,
-        dt.owner_id,
-        ou.username AS owner_username,
-        ou.full_name AS owner_full_name,
-        rtt.category_id AS task_category_id,
-        tc.name AS task_category,
-        dt.task_type_id,
-        rtt.name AS task_type,
-        dt.status_id,
-        CASE
-            WHEN ts.open_status = 0 THEN true
-            ELSE false
-            END AS is_open,
-        CASE
-            WHEN ts.open_status = 2 THEN true
-            ELSE false
-            END AS is_closed,
-        ts.name AS task_status,
-        dt.priority_id,
-        p.name AS task_priority,
-        dt.markup_type_id,
-        smt.name AS markup_type,
-        dt.desired_start_severity_id,
-        sds.name AS desired_start_severity,
-        dt.desired_end_severity_id,
-        eds.name AS desired_end_severity,
-        dt.desired_start,
-        dt.desired_end,
-        dt.estimated_start,
-        dt.estimated_end,
-        dt.actual_start,
-        dt.actual_end,
-        dt.time_estimate,
-        dt.description_markup,
-        dt.description_html,
-        dt.created_dt,
-        dt.created_by,
-        dt.updated_dt,
-        dt.updated_by,
-        cu.username AS created_username,
-        cu.full_name AS created_full_name,
-        uu.username AS updated_username,
-        uu.full_name AS updated_full_name
-    FROM tasker.dt_task dt
-    JOIN tasker.dv_activity_tree toc
-        ON ( toc.task_id = dt.id )
-    JOIN tasker.rt_task_type rtt
-        ON ( rtt.id = dt.task_type_id )
-    JOIN tasker.st_task_category tc
-        ON ( tc.id = rtt.category_id )
-    JOIN tasker.st_visibility sv
-        ON ( sv.id = dt.visibility_id )
-    JOIN tasker.dt_user ou
-        ON ( ou.id = dt.created_by )
-    LEFT JOIN tasker.st_date_severity sds
-        ON ( dt.desired_start_severity_id = sds.id )
-    LEFT JOIN tasker.st_date_severity eds
-        ON ( dt.desired_start_severity_id = eds.id )
-    LEFT JOIN tasker.st_ranking p
-        ON ( p.id = dt.priority_id )
-    LEFT JOIN tasker.rt_task_status ts
-        ON ( ts.id = dt.status_id )
-    LEFT JOIN tasker.st_markup_type smt
-        ON ( smt.id = dt.markup_type_id )
-    LEFT JOIN tasker.dt_user cu
-        ON ( cu.id = dt.created_by )
-    LEFT JOIN tasker.dt_user uu
-        ON ( uu.id = dt.updated_by ) ;
+SELECT base.id,
+        base.parent_id,
+        tree.parents,
+        tree.activity_depth,
+        tree.activity_path,
+        tree.outline_label,
+        base.edition,
+        base.task_name AS activity_name,
+        base.owner_id,
+        t006.username AS owner,
+        base.task_type_id AS activity_type_id,
+        t008.name AS activity_type,
+        base.visibility_id,
+        t010.name AS visibility,
+        base.markup_type_id,
+        t011.name AS markup_type,
+        base.status_id,
+        t012.name AS status,
+        t012.open_category_id,
+        t012.open_category,
+        base.priority_id,
+        t013.name AS priority,
+        base.desired_start_importance_id,
+        t014.name AS desired_start_importance,
+        base.desired_end_importance_id,
+        t015.name AS desired_end_importance,
+        base.desired_start,
+        base.desired_end,
+        base.estimated_start,
+        base.estimated_end,
+        base.actual_start,
+        base.actual_end,
+        base.description_markup,
+        base.description_html,
+        base.created_by_id,
+        cu.username AS created_by,
+        base.updated_by_id,
+        uu.username AS updated_by,
+        base.created_dt,
+        base.updated_dt
+    FROM tasker_data.dt_task base
+    JOIN tasker.dv_activity_tree tree
+        ON ( tree.id = base.id )
+    LEFT JOIN tasker_data.dt_user t006
+        ON ( t006.id = base.owner_id )
+    JOIN tasker_data.rt_task_type t008
+        ON ( t008.id = base.task_type_id )
+    JOIN tasker_data.st_visibility t010
+        ON ( t010.id = base.visibility_id )
+    JOIN tasker_data.st_markup_type t011
+        ON ( t011.id = base.markup_type_id )
+    LEFT JOIN tasker.rv_task_status t012
+        ON ( t012.id = base.status_id )
+    LEFT JOIN tasker_data.st_ranking t013
+        ON ( t013.id = base.priority_id )
+    LEFT JOIN tasker_data.st_date_importance t014
+        ON ( t014.id = base.desired_start_importance_id )
+    LEFT JOIN tasker_data.st_date_importance t015
+        ON ( t015.id = base.desired_end_importance_id )
+    LEFT JOIN tasker_data.dt_user cu
+        ON ( cu.id = base.created_by_id )
+    LEFT JOIN tasker_data.dt_user uu
+        ON ( uu.id = base.updated_by_id )
+    WHERE base.id = base.activity_id ;
 
-ALTER TABLE dv_activity OWNER TO tasker_owner ;
+ALTER VIEW tasker.dv_activity OWNER TO tasker_owner ;
 
-COMMENT ON VIEW dv_activity IS 'Data view for activities.' ;
+GRANT SELECT ON tasker.dv_activity TO tasker_user ;
 
-REVOKE ALL ON TABLE dv_activity FROM public ;
-
-GRANT SELECT ON table dv_activity TO tasker_owner ;
-
-GRANT SELECT ON table dv_activity TO tasker_user ;
+COMMENT ON VIEW tasker.dv_activity IS 'View of: activities.' ;
+COMMENT ON COLUMN tasker.dv_activity.id IS 'The unique ID for the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.parent_id IS 'The ID of the parent activity (if any).' ;
+COMMENT ON COLUMN tasker.dv_activity.parents IS 'The array of parent activity IDs.' ;
+COMMENT ON COLUMN tasker.dv_activity.activity_depth IS 'Indicates how far the activity is from the tree root' ;
+COMMENT ON COLUMN tasker.dv_activity.activity_path IS 'The array of node indexes in the tree (mostly for sorting purposes)' ;
+COMMENT ON COLUMN tasker.dv_activity.outline_label IS 'The (decimal dotted notation) label for the activity hierarchy outline.' ;
+COMMENT ON COLUMN tasker.dv_activity.edition IS 'Indicates the number of edits made to the activity. Intended for use in determining if an activity has been edited between select and update.' ;
+COMMENT ON COLUMN tasker.dv_activity.activity_name IS 'The name for the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.owner_id IS 'The ID of the user that owns the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.owner IS 'The username for the owner' ;
+COMMENT ON COLUMN tasker.dv_activity.activity_type_id IS 'The ID of the activity type.' ;
+COMMENT ON COLUMN tasker.dv_activity.activity_type IS 'The name for the activity type' ;
+COMMENT ON COLUMN tasker.dv_activity.visibility_id IS 'Indicates the visibility of the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.visibility IS 'The name for the visibility' ;
+COMMENT ON COLUMN tasker.dv_activity.markup_type_id IS 'The ID of the markup format used for the description_markup column.' ;
+COMMENT ON COLUMN tasker.dv_activity.markup_type IS 'The name for the markup type' ;
+COMMENT ON COLUMN tasker.dv_activity.status_id IS 'The status of the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.status IS 'The name of the status.' ;
+COMMENT ON COLUMN tasker.dv_activity.open_category_id IS 'The ID of the category indicating if the status is open, closed, or not open.' ;
+COMMENT ON COLUMN tasker.dv_activity.open_category IS 'The name for the open category' ;
+COMMENT ON COLUMN tasker.dv_activity.priority_id IS 'The priority of the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.priority IS 'The name for the priority' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_start_importance_id IS 'The importance of not making the desired start date.' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_start_importance IS 'The name for the desired start importance' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_end_importance_id IS 'The importance of not making the desired end date.' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_end_importance IS 'The name for the desired end importance' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_start IS 'The desired date (if any) that work on the activity should start.' ;
+COMMENT ON COLUMN tasker.dv_activity.desired_end IS 'The desired date (if any) for the completion for the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.estimated_start IS 'The estimated date (if any) that work on the activity should start.' ;
+COMMENT ON COLUMN tasker.dv_activity.estimated_end IS 'The estimated date (if any) for the completion for the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.actual_start IS 'The actual date that work on the activity was started.' ;
+COMMENT ON COLUMN tasker.dv_activity.actual_end IS 'The actual date that the activity was finished.' ;
+COMMENT ON COLUMN tasker.dv_activity.description_markup IS 'A description of the activity and/or the purpose of the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity.description_html IS 'The description in HTML format.' ;
+COMMENT ON COLUMN tasker.dv_activity.created_by_id IS 'The ID of the individual that created the row (ref pt_user).' ;
+COMMENT ON COLUMN tasker.dv_activity.created_by IS 'The username of the individual that created the row (ref pt_user).' ;
+COMMENT ON COLUMN tasker.dv_activity.updated_by_id IS 'The ID of the individual that most recently updated the row (ref pt_user).' ;
+COMMENT ON COLUMN tasker.dv_activity.updated_by IS 'The username of the individual that most recently updated the row (ref pt_user).' ;
+COMMENT ON COLUMN tasker.dv_activity.created_dt IS 'The timestamp when the row was created.' ;
+COMMENT ON COLUMN tasker.dv_activity.updated_dt IS 'The timestamp when the row was most recently updated.' ;
--- pg/tasker/view/dv_activity_tree.sql
+++ pg/tasker/view/dv_activity_tree.sql
@@ -1,46 +1,48 @@
-SET search_path = tasker, pg_catalog ;
-
-CREATE VIEW dv_activity_tree
+CREATE OR REPLACE VIEW tasker.dv_activity_tree
 AS
 WITH RECURSIVE toc AS (
     SELECT base.id,
             '{}'::int[] AS parents,
             base.parent_id,
-            0 AS task_depth,
+            0 AS activity_depth,
             ARRAY [ dense_rank () OVER (
                 PARTITION BY base.activity_id
                 ORDER BY base.id ) ] AS outln,
-            ARRAY[base.id] AS task_path
-        FROM tasker.dt_task base
+            ARRAY[base.id] AS activity_path
+        FROM tasker_data.dt_task base
         WHERE base.activity_id = base.id -- that are activities
             AND base.parent_id IS NULL
     UNION ALL
     SELECT base.id,
             parents || base.parent_id,
             base.parent_id,
-            ( q.task_depth + 1 ) AS task_depth,
+            ( q.activity_depth + 1 ) AS activity_depth,
             ( q.outln || dense_rank () OVER (
                     PARTITION BY base.activity_id, base.parent_id
                     ORDER BY base.id ) ) AS outln,
-            q.task_path || base.id
-        FROM tasker.dt_task base
+            q.activity_path || base.id
+        FROM tasker_data.dt_task base
         JOIN toc q
             ON ( base.parent_id = q.id
                 AND base.activity_id = base.id -- is activity
                 AND NOT base.id = ANY ( q.parents ) ) -- avoid cyclic references
 )
-SELECT toc.id AS task_id,
+SELECT toc.id,
         toc.parent_id,
         toc.parents,
-        toc.task_depth,
-        toc.task_path,
-        array_to_string ( toc.outln, '.'::text ) AS task_outln
+        toc.activity_depth,
+        toc.activity_path,
+        array_to_string ( toc.outln, '.'::text ) AS outline_label
     FROM toc ;
 
-ALTER TABLE dv_activity_tree OWNER TO tasker_owner ;
+ALTER TABLE tasker.dv_activity_tree OWNER TO tasker_owner ;
 
-COMMENT ON VIEW dv_activity_tree IS 'View of the hierarchy of activities.' ;
+GRANT SELECT ON tasker.dv_activity_tree TO tasker_owner ;
 
-REVOKE ALL ON TABLE dv_activity_tree FROM public ;
-
-GRANT SELECT ON table dv_activity_tree TO tasker_owner ;
+COMMENT ON VIEW tasker.dv_activity_tree IS 'View of the hierarchy of activities.' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.id IS 'The unique ID for the activity.' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.parent_id IS 'The ID for the parent activity.' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.parents IS 'The array of parent activity IDs.' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.activity_depth IS 'Indicates how far the activity is from the tree root' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.activity_path IS 'The array of node indexes in the tree (mostly for sorting purposes)' ;
+COMMENT ON COLUMN tasker.dv_activity_tree.outline_label IS 'The (decimal dotted notation) label for the activity hierarchy outline.' ;
